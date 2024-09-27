02fd0ca86e8ab915bbd11591040e64a2d1603b87
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2024-09-26 23:20:25 -0400

    added driver.py to use library

---
+++ driver.py
@@ -0,0 +1,61 @@
+import random
+import string
+
+from ciphers import Caesar, Hill, Playfair, Vatsyayana, Affine
+
+all_chars = string.ascii_letters + string.digits
+TOTAL_LENGTH = 100
+message_len = lambda message: sum([char.isalnum() for char in message])
+
+
+def print_message(text, cipher):
+    print("Encoded message:", encoded := cipher.encode(message))
+    print("Decoded message:", cipher.decode(encoded))
+
+
+print(f"\n{' Sample Text ':#^{TOTAL_LENGTH}}\n")
+message = "Ciphers are amazing. They can encode any piece of text."
+print(message)
+
+print(f"\n{' Monoalphabetic Ciphers ':#^{TOTAL_LENGTH}}")
+print(f"{' Caesar ':#^{TOTAL_LENGTH}}\n")
+caesar = Caesar(5)
+print_message(message, caesar)
+
+
+def create_pairs(s):
+    # Convert the string to a list of characters
+    characters = list(s)
+    pairs = []
+
+    # Continue until all characters are paired
+    while characters:
+        # Randomly select two characters
+        pair = random.sample(characters, 2)
+        pairs.append(tuple(pair))
+        # Remove the selected characters from the list
+        characters.remove(pair[0])
+        characters.remove(pair[1])
+
+    return pairs
+
+
+print(f"\n{' Vatsyayana ':#^{TOTAL_LENGTH}}\n")
+pairs = create_pairs(all_chars)
+vatsyayana = Vatsyayana(pairs)
+print_message(message, vatsyayana)
+
+print(f"\n{' Affine ':#^{TOTAL_LENGTH}}\n")
+affine = Affine(9, 3)
+print_message(message, affine)
+
+print(f"\n{' Polyalphabetic Ciphers ':#^{TOTAL_LENGTH}}")
+print(f"{' Playfair ':#^{TOTAL_LENGTH}}\n")
+passphrase = "221B"
+playfair = Playfair(passphrase)
+print_message(message, playfair)
+
+print(f"\n{' Hill ':#^{TOTAL_LENGTH}}\n")
+key = "DFCH"
+hill = Hill(key)
+print_message(message, hill)
