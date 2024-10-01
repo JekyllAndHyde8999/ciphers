ab76b0036e708179dd36a083b909d6cccc8dfe84
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2024-10-01 13:56:22 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ciphers",
+    packages=find_packages(),
+    version="0.1",
+    description="A library containing implementations of various ciphers. Just import the cipher and call the encode and decode methods.",
+    author="Sravan Vinakota"
+)
