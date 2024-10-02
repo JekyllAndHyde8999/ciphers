514e4b455b59765301f06f250b9722872ada8edb
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2024-10-02 15:59:56 -0400

    added function to separate alphanum and non-alphanum chars

--- ciphers/base.py
+++ ciphers/base.py
@@ -5,6 +5,17 @@ class Cipher:
     def __init__(self) -> None:
         self.letters = string.ascii_letters + string.digits
 
+    def separate(self, message: str) -> tuple[list[str], list[tuple[str, int]]]:
+        message_wo_puncts = []
+        puncts = []
+        for i, char in enumerate(message):
+            if char in self.letters:
+                message_wo_puncts.append(char)
+            else:
+                puncts.append((char, i))
+
+        return message_wo_puncts, puncts
+
     def encode(self, message: str = None) -> str:
         raise NotImplementedError("Method `encode` is not implemented")
 
