6fd0be9cd8a4ddc56ba94d07d80c3f830ebd8b50
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2025-07-31 22:24:31 -0400

    Added docstring for separate method

--- ciphers/base.py
+++ ciphers/base.py
@@ -6,6 +6,9 @@ class Cipher:
         self.letters = string.ascii_letters + string.digits
 
     def separate(self, message: str) -> tuple[list[str], list[tuple[str, int]]]:
+        """
+        Split message into alphanum characters and punctuation characters
+        """
         message_wo_puncts = []
         puncts = []
         for i, char in enumerate(message):
