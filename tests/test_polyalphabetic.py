ca2087473c80c3d9cf79844fe4360b81e52fef97
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2025-08-03 00:23:34 -0400

    Added test case for Vigenere cipher

--- tests/test_polyalphabetic.py
+++ tests/test_polyalphabetic.py
@@ -1,7 +1,7 @@
 import unittest
 
 from ciphers.base import Cipher
-from ciphers.polyalphabetic import AutoKey
+from ciphers.polyalphabetic import AutoKey, Vigenere
 
 
 class TestAutoKeyCipher(unittest.TestCase):
@@ -11,7 +11,34 @@ class TestAutoKeyCipher(unittest.TestCase):
         """
 
         autokey_cipher: Cipher = AutoKey("KEY")
-        plain_text:str = 'Hello'
-        encrypted_text:str = 'hI9Ss'
+        plain_text: str = "Hello"
+        encrypted_text: str = "hI9Ss"
 
         self.assertEqual(autokey_cipher.encode(plain_text), encrypted_text)
+        self.assertEqual(autokey_cipher.decode(encrypted_text), plain_text)
+
+
+class TestVigenereCipher(unittest.TestCase):
+    def test_simple_string(self):
+        """
+        Testing encryption/decryption on message containing only alphabet
+        """
+
+        vigenere_cipher: Cipher = Vigenere("KEY")
+        plain_text: str = "Hello"
+        encrypted_text: str = "hI9VS"
+
+        self.assertEqual(vigenere_cipher.encode(plain_text), encrypted_text)
+        self.assertEqual(vigenere_cipher.decode(encrypted_text), plain_text)
+
+    def test_string_with_alphanum(self):
+        """
+        Testing encryption/decryption on message with alphanumeric characters
+        """
+
+        vigenere_cipher: Cipher = Vigenere("KEY")
+        plain_text: str = "Hello123"
+        encrypted_text: str = "hI9VSPCx"
+
+        self.assertEqual(vigenere_cipher.encode(plain_text), encrypted_text)
+        self.assertEqual(vigenere_cipher.decode(encrypted_text), plain_text)
