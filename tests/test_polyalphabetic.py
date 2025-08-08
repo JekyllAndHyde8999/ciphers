b0b20af6b7a9725d7079db4e255c2591e8187c6c
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2025-08-07 22:49:22 -0400

    Added testcase for Hill cipher

--- tests/test_polyalphabetic.py
+++ tests/test_polyalphabetic.py
@@ -1,7 +1,8 @@
 import unittest
 
 from ciphers.base import Cipher
-from ciphers.polyalphabetic import AutoKey, Vigenere
+from ciphers.exceptions import InvalidKeyLength
+from ciphers.polyalphabetic import AutoKey, Hill, Vigenere
 
 
 class TestAutoKeyCipher(unittest.TestCase):
@@ -42,3 +43,23 @@ class TestVigenereCipher(unittest.TestCase):
 
         self.assertEqual(vigenere_cipher.encode(plain_text), encrypted_text)
         self.assertEqual(vigenere_cipher.decode(encrypted_text), plain_text)
+
+
+class TestHillCipher(unittest.TestCase):
+    def test_simple_string(self):
+        """
+        Testing encryption/decryption on message containing only alphabet
+        """
+        hill_cipher: Cipher = Hill("HILL")
+        plain_text: str = "Hello"
+        encrypted_text: str = "Vf3iuL"
+
+        self.assertEqual(hill_cipher.encode(plain_text), encrypted_text)
+        self.assertEqual(hill_cipher.decode(encrypted_text), plain_text)
+
+    def test_error_raises(self):
+        """
+        Length of key must be perfect square
+        """
+        with self.assertRaises(InvalidKeyLength):
+            Hill("LEY")
